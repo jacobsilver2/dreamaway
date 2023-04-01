13cfb418e176aa180deba423aae7cfad89e9c73f
Jacob Silver <jsilver@tempo.io>
2023-04-01 12:46:30 -0400

    fix no default export in about pagels

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import { AboutLayout } from "../components/common/layouts/AboutLayout";
 import { NextPageWithLayout } from "./_app";
 
-export const About: NextPageWithLayout = () => {
+const About: NextPageWithLayout = () => {
   return <div>About</div>;
 };
 
 About.getLayout = (page) => {
   return <AboutLayout>{page}</AboutLayout>;
 };
+
+export default About;
