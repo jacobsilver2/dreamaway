a24ebf108ee0651aff31ec46468a97b74bd64b6d
Jacob Silver <jsilver@tempo.io>
2023-04-10 15:09:44 -0400

    add more to middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,15 @@ import { get } from "@vercel/edge-config";
 export const config = {
   // have the matcher match every path except the maintenance page
   // this is to prevent an infinite loop
-  matcher: ["/((?!coming-soon).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|favicon.ico|public|coming-soon).*)",
+  ],
 };
 
 export async function middleware(req: NextRequest) {
